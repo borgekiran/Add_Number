5d032634c3c0508b462ae2bac6395e54ff73c11e
Kiran Borge <78689154+borgekiran@users.noreply.github.com>
2022-10-05 05:05:07 -0700

    javascript

---
+++ add_num/add_num.js
@@ -0,0 +1,5 @@
+let a = prompt("please enter first number", "");
+        let b = prompt("please enter Second number", "");
+
+        var sum = a / b;
+        console.log("The Division of two number : " + a + " and " + b + " is:" + sum);
